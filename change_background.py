7c0e2811d23814dfc71bc9ea5dee1776420ae6e7
konga400 <38776747+konga400@users.noreply.github.com>
2020-07-20 00:28:56 -0400

    Updated

--- change_background.py
+++ change_background.py
@@ -1,28 +1,42 @@
-        import ctypes
-        import os
-        import sys
-        from random import randrange
+import ctypes
+import os
+from random import randrange
+
+dir_name = "Pictures"
+pathname = os.getcwd()
+pathname_pic = pathname + "/{}".format(dir_name)
+if not os.path.exists(pathname_pic):
+    os.mkdir(pathname_pic)
+    print("Created directory called \"{}\". Place pictures inside new directory".format(dir_name))
 
 
-        x = randrange(3)
+files_in_dir = []
 
-        pathname = os.path.dirname(sys.argv[0])        
-        pathname.replace("change_background.py", "")
-        pathname = pathname + "/Pictures/"
+# r=>root, d=>directories, f=>files
+# this grabs all of the images in the directory
+for r, d, f in os.walk(pathname_pic):
+   for item in f:
+      if '.jpg' in item:
+         files_in_dir.append(os.path.join(r, item))
+      if '.png' in item:
+         files_in_dir.append(os.path.join(r, item))
 
-        def change_pic(image_name):
-            pathToJpg = os.path.normpath("{}{}".format(pathname, image_name))
-            SPI_SETDESKWALLPAPER = 20
-            ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, pathToJpg, 0)
 
-        if x == 0:
-            change_pic("image1.jpg")
+def change_pic():
+   if len(files_in_dir) == 0:
+      print("You don't have any pictures in your directory")
+   else:
+      x = randrange(len(files_in_dir))
+      print("Picture chosen: {}".format(files_in_dir[x]))
+      pathToJpg = files_in_dir[x]
+      SPI_SETDESKWALLPAPER = 20
+      ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, pathToJpg, 0)
+      x = x - 1
 
-        if x == 1:
-            change_pic("image2.jpg")
 
-        if x == 2:
-            change_pic("image3.jpg")
+if __name__ == "__main__":
+   change_pic()
+
 
 
 
